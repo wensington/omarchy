634f628dd855c4af42d6872c024a3c963c860a71
Wen Tian <wen@wensington.com>
2025-07-19 12:11:37 -0400

    Add additional development packages

--- install/development.sh
+++ install/development.sh
@@ -3,6 +3,6 @@
 yay -S --noconfirm --needed \
   cargo clang llvm mise \
   imagemagick \
-  mariadb-libs postgresql-libs \
   github-cli \
-  lazygit lazydocker-bin
+  lazygit lazydocker-bin \
+  s5cmd hugo
